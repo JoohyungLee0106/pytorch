06349db9cd2ce9499c4584249615f3194f2797e5
JoohyungLee0106 <43436960+JoohyungLee0106@users.noreply.github.com>
2020-04-14 20:34:44 +0900

    Update main.py

--- medical_image_classification_attention/main.py
+++ medical_image_classification_attention/main.py
@@ -21,61 +21,10 @@ FOLD_SIZE=10
 OUTPUT_NUM = 3
 # resnet18, resnet34, resnet50, resnext50, attention_dilated_2D, attention_dilated_with_cancer_2D
 NETWORK = networks.attention_dilated_with_cancer_2D
-###########################################################################################################
 SAVE_FOLDER_NAME = 'avp_16'
 FEATURE_NUM=16
 MINI_BATCH_SIZE = 16
 PARAMS_NETWORK = {'pretrained':False,'dilations':True, 'global_pooling': 'avp','arch':'resnet34', 'block':networks.BasicBlock,
-                  'base_width':FEATURE_NUM,'starting_filter_num':FEATURE_NUM, 'lated': False, 'my_device': torch.device(MY_DEVICE)}
-
-if __name__ == '__main__':
-    res34_train = training.Training(path_csv_before_fold_num_tr=CSV_TR, path_csv_before_fold_num_val=CSV_VAL,
-                           result_save_directory=RESULT_SAVE_DIRECTORY, save_folder_name=SAVE_FOLDER_NAME,
-                           my_device=MY_DEVICE, mini_batch_size=MINI_BATCH_SIZE, network=NETWORK,
-                           params_network= PARAMS_NETWORK, output_num=OUTPUT_NUM, num_workers= NUM_WORKERS,
-                            max_epoch=MAX_EPOCH, fold_size=FOLD_SIZE)
-
-    res34_train()
-
-###########################################################################################################
-SAVE_FOLDER_NAME = 'mxp_16'
-FEATURE_NUM=16
-MINI_BATCH_SIZE = 16
-PARAMS_NETWORK = {'pretrained':False,'dilations':True, 'global_pooling': 'mxp','arch':'resnet34', 'block':networks.BasicBlock,
-                  'base_width':FEATURE_NUM,'starting_filter_num':FEATURE_NUM, 'lated': False, 'my_device': torch.device(MY_DEVICE)}
-
-if __name__ == '__main__':
-    res34_train = training.Training(path_csv_before_fold_num_tr=CSV_TR, path_csv_before_fold_num_val=CSV_VAL,
-                           result_save_directory=RESULT_SAVE_DIRECTORY, save_folder_name=SAVE_FOLDER_NAME,
-                           my_device=MY_DEVICE, mini_batch_size=MINI_BATCH_SIZE, network=NETWORK,
-                           params_network= PARAMS_NETWORK, output_num=OUTPUT_NUM, num_workers= NUM_WORKERS,
-                            max_epoch=MAX_EPOCH, fold_size=FOLD_SIZE)
-
-    res34_train()
-
-
-###########################################################################################################
-
-SAVE_FOLDER_NAME = 'avp_64prt'
-FEATURE_NUM=64
-MINI_BATCH_SIZE = 8
-PARAMS_NETWORK = {'pretrained':True,'dilations':True, 'global_pooling': 'avp','arch':'resnet34', 'block':networks.BasicBlock,
-                  'base_width':FEATURE_NUM,'starting_filter_num':FEATURE_NUM, 'lated': False, 'my_device': torch.device(MY_DEVICE)}
-
-if __name__ == '__main__':
-    res34_train = training.Training(path_csv_before_fold_num_tr=CSV_TR, path_csv_before_fold_num_val=CSV_VAL,
-                           result_save_directory=RESULT_SAVE_DIRECTORY, save_folder_name=SAVE_FOLDER_NAME,
-                           my_device=MY_DEVICE, mini_batch_size=MINI_BATCH_SIZE, network=NETWORK,
-                           params_network= PARAMS_NETWORK, output_num=OUTPUT_NUM, num_workers= NUM_WORKERS,
-                            max_epoch=MAX_EPOCH, fold_size=FOLD_SIZE)
-
-    res34_train()
-
-###########################################################################################################
-SAVE_FOLDER_NAME = 'mxp_64prt'
-FEATURE_NUM=64
-MINI_BATCH_SIZE = 8
-PARAMS_NETWORK = {'pretrained':True,'dilations':True, 'global_pooling': 'mxp','arch':'resnet34', 'block':networks.BasicBlock,
                   'base_width':FEATURE_NUM,'starting_filter_num':FEATURE_NUM, 'lated': False, 'my_device': torch.device(MY_DEVICE)}
 
 if __name__ == '__main__':
